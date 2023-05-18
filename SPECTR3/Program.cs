6e74706af6d4108e50e03059bc954337d3df1a1f
kero99 <motrilwireless@gmail.com>
2023-05-18 23:32:43 +0200

    Utils functions reorganization and clean up

--- SPECTR3/Program.cs
+++ SPECTR3/Program.cs
@@ -12,22 +12,14 @@
  * either version 3 of the License, or (at your option) any later version.
  */
 
-
 using System;
-using System.Text;
 using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Net;
-using System.Net.NetworkInformation;
-using DiskAccessLibrary;
-using DiskAccessLibrary.LogicalDiskManager;
 using DiskAccessLibrary.Win32;
 using Utilities;
 using ISCSI.Server;
-using System.Security.Principal;
-using System.Text.RegularExpressions;
 using System.Threading;
-using System.Linq;
 
 namespace SPECTR3
 {
@@ -37,287 +29,9 @@ namespace SPECTR3
 
         public const string DefaultTargetIQN = "iqn.2023-05.io.alpine";
 
-        public class VolumeDisk : Disk // a fake disk that serves a single volume
-        {
-            private Volume m_volume;
-            private bool m_isReadOnly;
-
-            public VolumeDisk(Volume volume, bool isReadOnly)
-            {
-                m_volume = volume;
-                m_isReadOnly = volume.IsReadOnly || isReadOnly;
-            }
-
-            public override byte[] ReadSectors(long sectorIndex, int sectorCount)
-            {
-                return m_volume.ReadSectors(sectorIndex, sectorCount);
-            }
-
-            public override void WriteSectors(long sectorIndex, byte[] data)
-            {
-                if (!IsReadOnly)
-                {
-                    m_volume.WriteSectors(sectorIndex, data);
-                }
-            }
-
-            public override int BytesPerSector
-            {
-                get
-                {
-                    return m_volume.BytesPerSector;
-                }
-            }
-
-            public override long Size
-            {
-                get
-                {
-                    return m_volume.Size;
-                }
-            }
-
-            public override bool IsReadOnly
-            {
-                get
-                {
-                    return m_isReadOnly;
-                }
-            }
-
-            public Volume Volume
-            {
-                get
-                {
-                    return m_volume;
-                }
-            }
-        }
-
-        public class SecurityHelper
-        {
-            public static bool IsAdministrator()
-            {
-                WindowsIdentity windowsIdentity = null;
-                try
-                {
-                    windowsIdentity = WindowsIdentity.GetCurrent();
-                }
-                catch
-                {
-                    return false;
-                }
-                WindowsPrincipal windowsPrincipal = new WindowsPrincipal(windowsIdentity);
-                return windowsPrincipal.IsInRole(WindowsBuiltInRole.Administrator);
-            }
-        }
-
-        public static bool IsBase64String(String s)
-        {
-            s = s.Trim();
-            return (s.Length % 4 == 0) && Regex.IsMatch(s, @"^[a-zA-Z0-9\+/]*={0,3}$", RegexOptions.None);
-
-        }
-
-        public static string Base64Decode(string base64EncodedData)
-        {
-            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
-            return Encoding.UTF8.GetString(base64EncodedBytes);
-        }
-
-        // Get random phrases from a messages list
-        public static string GetRandomMessage()
-        {
-            string[] messages = new string[] { "What's the problem with having meetings at 3 a.m.?",
-                                               "I invented that 10 years ago",
-                                               "Furest already does",
-                                               "Trinito already does",
-                                               "The infrastructure never falls off",
-                                               "Cool EVERYTHING",
-                                               "My twitter profile on the front page!",
-                                               "That's not how we do things here",
-                                               "Automate, Automate, Automate!",
-                                               "Real World",
-                                               "Not Trivial",
-                                               "We have to show our chest",
-                                               "This is a game changer",
-                                               "This was born after a brain storm meeting at 3 am",
-                                               "Is this Cloud free?",
-                                               "This tool was designed 10 years ago",
-                                               "This is the result of years of automation",
-                                               "I'll take this in one night",
-                                               "It is just a IF",
-                                               "We need a mind blowing presentation",
-                                               "Nobody told me the code had to be secure"};
-            Random rnd = new Random();
-            int index = rnd.Next(messages.Length);
-            return messages[index];
-        }
-
-        //Get the interface ip address with gateway and not localhost
-        public static string GetLocalIPAddress()
-        {
-            string ipaddress = "";
-            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
-            {
-                if (ni.NetworkInterfaceType == NetworkInterfaceType.Ethernet && ni.OperationalStatus == OperationalStatus.Up)
-                {
-                    foreach (GatewayIPAddressInformation gipi in ni.GetIPProperties().GatewayAddresses)
-                    {
-                        if (gipi.Address.AddressFamily == AddressFamily.InterNetwork)
-                        {
-                            ipaddress = ni.GetIPProperties().UnicastAddresses[1].Address.ToString();
-                        }
-                    }
-                }
-                // Get the interface ip address for wifi conections if ethernet is not available
-                if (string.IsNullOrEmpty(ipaddress) && ni.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 && ni.OperationalStatus == OperationalStatus.Up)
-                {
-                    foreach (GatewayIPAddressInformation gipi in ni.GetIPProperties().GatewayAddresses)
-                    {
-                        if (gipi.Address.AddressFamily == AddressFamily.InterNetwork)
-                        {
-                            ipaddress = ni.GetIPProperties().UnicastAddresses[1].Address.ToString();
-                        }
-                    }
-                }
-            }
-            return ipaddress;
-        }
-
-        public static bool CheckIP(string ipAddress)
-        {
-            foreach (NetworkInterface networkInterface in NetworkInterface.GetAllNetworkInterfaces())
-            {
-                if (networkInterface.NetworkInterfaceType == NetworkInterfaceType.Ethernet && networkInterface.OperationalStatus == OperationalStatus.Up ||
-                    networkInterface.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 && networkInterface.OperationalStatus == OperationalStatus.Up)
-                {
-                    foreach (UnicastIPAddressInformation ipInfo in networkInterface.GetIPProperties().UnicastAddresses)
-                    {
-                        if (ipInfo.Address.AddressFamily == AddressFamily.InterNetwork &&
-                            IPAddress.Parse(ipAddress).Equals(ipInfo.Address))
-                        {
-                            return true;
-                        }
-                    }
-                }
-            }
-            return false;
-        }
-
-        public static bool ValidateIPv4(string ip)
-        {
-            IPAddress address;
-            return ip != null && ip.Count(c => c == '.') == 3 &&
-                IPAddress.TryParse(ip, out address);
-        }
-
-        static String BytesToString(long byteCount)
-        {
-            string[] suf = { "B", "KB", "MB", "GB", "TB", "PB", "EB" }; //Longs run out around EB
-            if (byteCount == 0)
-                return "0" + suf[0];
-            long bytes = Math.Abs(byteCount);
-            int place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
-            double num = Math.Round(bytes / Math.Pow(1024, place), 1);
-            return (Math.Sign(byteCount) * num).ToString() + suf[place];
-        }
-
-        public static List<string> VolumesList()
-        {
-            List<string> volumeList = new List<string>();
-
-            List<Volume> volumes = WindowsVolumeHelper.GetVolumes();
-            for (int i = 0; i < volumes.Count; i++)
-            {
-                Volume volume = volumes[i];
-                string title = String.Format("Vol {0}", i);
-                string type = VolumeHelper.GetVolumeTypeString(volume);
-                string status = VolumeHelper.GetVolumeStatusString(volume);
-
-                ulong volumeID = 0;
-                string name = String.Empty;
-
-                if (volume is DynamicVolume)
-                {
-                    var dynamicVolume = (DynamicVolume)volume;
-                    volumeID = dynamicVolume.VolumeID;
-                    name = dynamicVolume.Name;
-                }
-                else if (volume is GPTPartition)
-                {
-                    name = ((GPTPartition)volume).PartitionName;
-                }
-
-                if (string.IsNullOrEmpty(name))
-                {
-                    name = "Noname";
-                }
-
-                string thisSizeString = BytesToString(volume.Size);
-                string line = $"    + {title}:  {name} {type} {thisSizeString} {status}";
-                volumeList.Add(line);
-            }
-
-            return volumeList;
-        }
-
-        static List<string> DiskList()
-        {
-            List<string> dskstr = new List<string>();
-            List<PhysicalDisk> physicalDisks = PhysicalDiskHelper.GetPhysicalDisks();
-            foreach (PhysicalDisk physicalDisk in physicalDisks)
-            {
-                string title = String.Format("Dsk {0}", physicalDisk.PhysicalDiskIndex);
-                string description = physicalDisk.Description;
-                string serialNumber = physicalDisk.SerialNumber;
-                string sizeString = BytesToString(physicalDisk.Size);
-
-                string status = string.Empty;
-                string line;
-                try
-                {
-                    if (Environment.OSVersion.Version.Major > 6)
-                    {
-                        bool isOnline = physicalDisk.GetOnlineStatus();
-                        status = isOnline ? "Online" : "Offline";
-
-                    }
-                    
-                }
-                catch (Exception)
-                {
-                    status = "Unknown";
-                }
-                line = string.Format("    + {0}:  {1}  {2}  {3}  {4}", title, description, serialNumber, sizeString, status);
-                dskstr.Add(line);
-            }
-            return dskstr;
-        }
-
-
-
-        static void ShowDiskAndVolumes()
-        {
-            List<string> volstr;
-            List<string> dskstr;
-            Console.WriteLine("- List Physical Disks:");
-            dskstr = DiskList();
-            foreach (string dsk in dskstr)
-            {
-                Console.WriteLine(dsk);
-            }
-            Console.WriteLine("- List Volumes:");
-            volstr = VolumesList();
-            foreach (string vol in volstr)
-            {
-                Console.WriteLine(vol);
-            }
-        }
-
         private static void PrintHelp()
         {
-            Console.WriteLine("SPECTR3 v0.4.4 - Remote acquisition and forensic tool by Alpine Security");
+            Console.WriteLine("SPECTR3 v0.4.6 - Remote acquisition and forensic tool by Alpine Security");
             Console.WriteLine("Usage: SPECTR3.exe [options]");
             Console.WriteLine("Options:");
             Console.WriteLine("  -l, --list");
@@ -369,7 +83,7 @@ namespace SPECTR3
                  { "--list", "--port", "--permitip", "--bindip", "--volume", "--disk", "--help", "--sshuser",
                    "--sshpass", "--sshhost", "--sshport", "-l", "-p", "-i", "-b", "-h", "-v", "-d", "-o"};
 
-            if (!SecurityHelper.IsAdministrator())
+            if (!SP3UTILS.SecurityHelper.IsAdministrator())
             {
                 Console.WriteLine(" - Administrator privileges needed.");
                 return 1;
@@ -388,7 +102,7 @@ namespace SPECTR3
                 if (arg == "--list" || arg == "-l")
                 {
                     list = true;
-                    ShowDiskAndVolumes();
+                    SP3DSK.ShowDiskAndVolumes();
                     return 0;
                 }
 
@@ -406,7 +120,7 @@ namespace SPECTR3
                         return 1;
                     }
 
-                    if (!ValidateIPv4(args[i + 1]))
+                    if (!SP3NET.ValidateIPv4(args[i + 1]))
                     {
                         Console.WriteLine("  - Invalid Permited IP address");
                         return 1;
@@ -424,7 +138,7 @@ namespace SPECTR3
                     }
                     thisbind = args[i + 1];
 
-                    if (CheckIP(thisbind) == false)
+                    if (SP3NET.CheckIP(thisbind) == false)
                     {
                         Console.WriteLine("  - Invalid Bind IP address");
                         return 1;
@@ -498,7 +212,7 @@ namespace SPECTR3
                         return 1;
                     }
 
-                    if (!ValidateIPv4(args[i + 1]))
+                    if (!SP3NET.ValidateIPv4(args[i + 1]))
                     {
                         Console.WriteLine("  - Invalid SSH IP address");
                         return 1;
@@ -580,9 +294,9 @@ namespace SPECTR3
                 else
                 {
                     //Check if sshpass is base64 encoded
-                    if (IsBase64String(sshpass))
+                    if (SP3UTILS.IsBase64String(sshpass))
                     {
-                        sshpass = Base64Decode(sshpass).TrimEnd('\r', '\n'); ;
+                        sshpass = SP3UTILS.Base64Decode(sshpass).TrimEnd('\r', '\n'); ;
                     }
                     else
                     {
@@ -597,7 +311,7 @@ namespace SPECTR3
 
                 try
                 {
-                    serverIP = GetLocalIPAddress();
+                    serverIP = SP3NET.GetLocalIPAddress();
                 }
                 catch
                 {
@@ -635,14 +349,14 @@ namespace SPECTR3
             {
                 int volindex = Conversion.ToInt32(thisvolume);
                 drivename = String.Format("vol{0}", thisvolume);
-                VolumeDisk volumeDisk;
+                SP3DSK.VolumeDisk volumeDisk;
                 List<Volume> volumes = WindowsVolumeHelper.GetVolumes();
                 for (int index = 0; index < volumes.Count; index++)
                 {
                     if (index == volindex)
                     {
                         Volume volume = volumes[index];
-                        volumeDisk = new VolumeDisk(volume, true);
+                        volumeDisk = new SP3DSK.VolumeDisk(volume, true);
                         // Add the volume to the list of disks to share
                         m_disks.Add(volumeDisk);
                         break;
@@ -689,7 +403,7 @@ namespace SPECTR3
             if (thisegg)
             {
                 Console.WriteLine();
-                Console.WriteLine("  - Funny MOTD: " + GetRandomMessage());
+                Console.WriteLine("  - Funny MOTD: " + SP3UTILS.GetRandomMessage());
             }
 
             //Start Server
